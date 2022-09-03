a665900952b19ca619fd5d2cfc1f2c0c2ea798b7
Ed McCormick <edward.mccormick3@gmail.com>
2022-09-03 18:20:28 -0500

    voting decrements

--- judge.js
+++ judge.js
@@ -1,4 +1,5 @@
 let voteCount = 0;
+let votesRemaining;
 
 function increaseVotes() {
   const numVotes = document.getElementById("numVotes");
@@ -12,5 +13,24 @@ votebutton.addEventListener("click", () => {
   } else {
     voteCount++;
     increaseVotes();
+    displayRemainingVotes();
   }
 });
+
+function displayRemainingVotes() {
+  if (voteCount === 5) {
+    votesRemaining = 5;
+    const newDiv = document.createElement("div");
+    newDiv.setAttribute("id", "votesRemaining");
+    const newContent = document.createTextNode(
+      `You have ${votesRemaining} vote(s) remaining`
+    );
+    newDiv.append(newContent);
+    const currentDiv = document.getElementById("pagecontent");
+    document.body.insertBefore(newDiv, currentDiv);
+  } else if (voteCount > 5) {
+    votesRemaining -= 1;
+    let votesremainingid = document.getElementById("votesRemaining");
+    votesremainingid.innerHTML = `You have ${votesRemaining} vote(s) remaining`;
+  }
+}
